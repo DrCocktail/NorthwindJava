65c3e36cb448f8b4f376dac997fb0ef80d3643f0
DrCocktail <yigit.solak02@gmail.com>
2021-05-09 17:12:28 +0300

    First Commit

---
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -0,0 +1,8 @@
+package kodlamaio.northwind.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import kodlamaio.northwind.entities.conceretes.Product;
+
+public interface ProductDao extends JpaRepository<Product, Integer>{
+
+}
---
+++ northwind/src/main/java/kodlamaio/northwind/entities/conceretes/Product.java
@@ -0,0 +1,91 @@
+package kodlamaio.northwind.entities.conceretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="product_id")
+	private int id;
+	
+	@Column(name="category_id")
+	private int categoryId;
+	
+	@Column(name="product_name")
+	private String productName;
+	
+	@Column(name="unit_price")
+	private double unitprice;
+	
+	@Column(name="units_in_stock")
+	private short unitsInStock;
+	
+	@Column(name="quantity_per_unit")
+	private String quantityPerUnit;
+	
+	public Product(int id, int categoryId, String productName, double unitprice, short unitsInStock,
+			String quantityPerUnit) {
+		super();
+		this.id = id;
+		this.categoryId = categoryId;
+		this.productName = productName;
+		this.unitprice = unitprice;
+		this.unitsInStock = unitsInStock;
+		this.quantityPerUnit = quantityPerUnit;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getUnitprice() {
+		return unitprice;
+	}
+
+	public void setUnitprice(double unitprice) {
+		this.unitprice = unitprice;
+	}
+
+	public short getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(short unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public String getQuantityPerUnit() {
+		return quantityPerUnit;
+	}
+
+	public void setQuantityPerUnit(String quantityPerUnit) {
+		this.quantityPerUnit = quantityPerUnit;
+	}
+}
